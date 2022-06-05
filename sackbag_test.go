929e1b2ce8ef3ff0885afe025977bae0d114c4ed
zakhar.malinovskiy <zakhar.malinovskiy@gmail.com>
2022-06-05 18:49:46 +0300

    sackbag passes tests

--- sackbag_test.go
+++ sackbag_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+//Тест использует BruteForceSackbag чтобы проверить решение динамикой.
 func Test_sackbag(t *testing.T) {
 	type args struct {
 		W      float64
@@ -15,14 +16,16 @@ func Test_sackbag(t *testing.T) {
 		factor int //you dicide when and how to scale
 	}
 	tests := []struct {
-		name      string
-		args      args
-		want      int
-		wantitems []int
+		name string
+		args args
 	}{
 		// TODO: Add test cases.
-		{name: "1", args: args{W: 7, m: []float64{3, 2, 5, 1}, c: []int{10, 3, 18, 2}, factor: 1},
-			want: 21, wantitems: []int{2, 5}},
+		//W - это лимит рюкзака по весу.
+		//m - это веса предметов.
+		//c - это стоимости предметов.
+		//factor - число на которое надо умножить все веса чтобы они стали целыми числами.
+		{name: "int values", args: args{W: 7, m: []float64{3, 2, 5, 1}, c: []int{10, 3, 18, 2}, factor: 1}},
+		{name: "float values", args: args{W: 23.7, m: []float64{6, 15.8, 3.7, 7.7, 10.1, 17.7}, c: []int{10, 5, 23, 13, 8, 20}, factor: 10}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -31,13 +34,12 @@ func Test_sackbag(t *testing.T) {
 
 			//test values are scaled to be ints
 			mScaledInt := scaleToInts(tt.args.m, tt.args.factor)
-			WScaled := int(tt.args.W) * tt.args.factor
+			WScaled := int(tt.args.W * float64(tt.args.factor))
 
 			got, items := sackbag(WScaled, mScaledInt, tt.args.c)
-			gotfloat := float64(got / tt.args.factor) //scale back the answer
-			itemsfloat := scaleToFloats(items, 1.0/float64(tt.args.factor))
+			itemsfloat := scaleToFloats(items, 1.0/float64(tt.args.factor)) //scale back the answer
 
-			if gotfloat != want1 ||
+			if float64(got) != want1 ||
 				!reflect.DeepEqual(itemsfloat, want2) {
 				t.Errorf("sackbag() = %v, %v, want %v, %v", got, items, want1, want2)
 			}
